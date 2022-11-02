8678d3193a5a7276e8d25f77d4895ed96fbc4bcb
Carlo Casorzo <carlo@deepblu.dev>
2022-11-02 06:10:30 +0100

    fix: select min ttl instead of max

--- src/rate-limiter/rate-limiter.service.ts
+++ src/rate-limiter/rate-limiter.service.ts
@@ -6,7 +6,7 @@ import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common'
 export class RateLimiterService {
   constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
-  async checkLimits(token: string, opts: { limit: number; weight?: number }) {
+  async check(token: string, opts: { limit: number; weight?: number }) {
     const { limit, weight = 1 } = opts
 
     const keys = await this.cacheManager.store.keys(`rate-limit:${token}:*`)
@@ -18,19 +18,23 @@ export class RateLimiterService {
     const ttls = await Promise.all(
       keys.map(k => this.cacheManager.store.ttl(k))
     )
-    const secondsUntilLiftingLimit = Math.max(...ttls) || 0
 
+    const secondsUntilLiftingLimit = ttls.length ? Math.min(...ttls) : 0
     const totalWeight = values.reduce((acc, val) => acc + val.weight, 0)
     const isOverLimit = totalWeight > limit
 
-    const key = `rate-limit:${token}:${uuid()}`
-    if (!isOverLimit) await this.cacheManager.set(key, weight, { ttl: 60 * 60 }) // 1 hour
+    if (!isOverLimit)
+      await this.cacheManager.set(`rate-limit:${token}:${uuid()}`, weight, {
+        ttl: 60 * 60, // 1 hour
+      })
 
-    return {
+    const result = {
       token,
       isOverLimit,
       totalWeight,
       secondsUntilLiftingLimit,
     }
+
+    return result
   }
 }
